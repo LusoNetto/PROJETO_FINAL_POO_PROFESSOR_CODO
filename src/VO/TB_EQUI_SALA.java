e5317547dbd0dc40bcff09346bcc4d6513b5daed
unknown <luciano.silva137@hotmail.com>
2021-12-09 19:20:33 -0300

    concluindo algumas classes

--- src/VO/TB_EQUI_SALA.java
+++ src/VO/TB_EQUI_SALA.java
@@ -1,92 +1,35 @@
 package VO;
 
-public class TB_EQUI_SALA {
+public class TB_EQUI_SALA extends TB_AGENDAMENTO{
 	
-	private int CD_ID;
-    private String DT_DATA;
-    private String DT_HORA;
-	private int TB_TIPOAGENDAMENTO_CD_ID;
-	private int TB_FUNCIONARIO_CD_ID;
-	private int TB_EQUI_SALA_CD_ID;
-	private int TB_PACIENTE_CD_ID;
-	private int TB_ENTRADASAIDA_CD_ID;
+	private int CD_ID;	
+	private int TB_EQUIPAMENTO_CD_ID;
+	private int TB_SALA_CD_id_Sala;
+	
+	public TB_EQUI_SALA(int CD_ID) {		
+		super(CD_ID);
+		this.TB_EQUIPAMENTO_CD_ID = TB_EQUIPAMENTO_CD_ID;
+		this.TB_SALA_CD_id_Sala = TB_SALA_CD_id_Sala;
+	}
+	
+	public int getTB_EQUIPAMENTO_CD_ID() {
+		return this.TB_EQUIPAMENTO_CD_ID;
+	}
+	
+	public void setTB_EQUIPAMENTO_CD_ID(int TB_EQUIPAMENTO_CD_ID) {
+		this.TB_EQUIPAMENTO_CD_ID=TB_EQUIPAMENTO_CD_ID;
+	}
+	
+	public int getTB_SALA_CD_id_Sala() {
+		return this.TB_SALA_CD_id_Sala;
+	}
+	
+	public void setTB_SALA_CD_id_Sala(int TB_SALA_CD_id_Sala) {
+		this.TB_SALA_CD_id_Sala=TB_SALA_CD_id_Sala;
+	}
 
-    public void TB_AGENDAMENTO_PK(int CD_ID) {
+    public void TB_EQUI_SALA_PK(int CD_ID) {
     	
     }
-	
-	public int getCD_ID() {
-		return this.CD_ID;
-	}
-	
-	public void setCD_ID(int CD_ID) {
-		this.CD_ID=CD_ID;
-	}
-	
-	public String getDT_DATA() {
-		return this.DT_DATA;
-	}
-	
-	public void setDT_DATA(String DT_DATA) {
-		this.DT_DATA=DT_DATA;
-	}
-	
-	public String getDT_HORA() {
-		return this.DT_HORA;
-	}
-	
-	public void setDT_HORA(String DT_HORA) {
-		this.DT_HORA=DT_HORA;
-	}
-	
-	public int getTB_TIPOAGENDAMENTO_CD_ID() {
-		return this.TB_TIPOAGENDAMENTO_CD_ID;
-	}
-	
-	public void setTB_TIPOAGENDAMENTO_CD_ID(int TB_TIPOAGENDAMENTO_CD_ID) {
-		this.TB_TIPOAGENDAMENTO_CD_ID=TB_TIPOAGENDAMENTO_CD_ID;
-	}
-	
-	public int getTB_FUNCIONARIO_CD_ID() {
-		return this.TB_FUNCIONARIO_CD_ID;
-	}
-	
-	public void setTB_FUNCIONARIO_CD_ID(int TB_FUNCIONARIO_CD_ID) {
-		this.TB_FUNCIONARIO_CD_ID=TB_FUNCIONARIO_CD_ID;
-	}
-	
-	public int getTB_EQUI_SALA_CD_ID() {
-		return this.TB_EQUI_SALA_CD_ID;
-	}
-	
-	public void setTB_EQUI_SALA_CD_ID(int TB_EQUI_SALA_CD_ID) {
-		this.TB_EQUI_SALA_CD_ID=TB_EQUI_SALA_CD_ID;
-	}
-	
-	public int getTB_PACIENTE_CD_ID() {
-		return this.TB_PACIENTE_CD_ID;
-	}
-	
-	public void setTB_PACIENTE_CD_ID(int TB_PACIENTE_CD_ID) {
-		this.TB_PACIENTE_CD_ID=TB_PACIENTE_CD_ID;
-	}
-	
-	public int getTB_ENTRADASAIDA_CD_ID() {
-		return this.TB_ENTRADASAIDA_CD_ID;
-	}
-	
-	public void setTB_ENTRADASAIDA_CD_ID(int TB_ENTRADASAIDA_CD_ID) {
-		this.TB_ENTRADASAIDA_CD_ID=TB_ENTRADASAIDA_CD_ID;
-	}
-	public TB_TIPOAGENDAMENTO (int CD_ID, date DT_DATA, char DT_HORA, int TB_TIPOAGENDAMENTO_CD_ID, int TB_FUNCIONARIO_CD_ID, int TB_EQUI_SALA_CD_ID, int TB_PACIENTE_CD_ID, int TB_ENTRADASAIDA_CD_ID) {
-		this.CD_ID = CD_ID;
-		this.DT_DATA = DT_DATA;
-		this.DT_HORA = DT_HORA;
-		this.TB_TIPOAGENDAMENTO_CD_ID = TB_TIPOAGENDAMENTO_CD_ID;
-		this.TB_FUNCIONARIO_CD_ID = TB_FUNCIONARIO_CD_ID;
-		this.TB_EQUI_SALA_CD_ID = TB_EQUI_SALA_CD_ID;
-		this.TB_PACIENTE_CD_ID = TB_PACIENTE_CD_ID;
-		this.TB_ENTRADASAIDA_CD_ID = TB_ENTRADASAIDA_CD_ID;		
-	}
 
 }
--- src/VO/TB_SALA.java
+++ src/VO/TB_SALA.java
@@ -1,92 +1,46 @@
 package VO;
 
-public class TB_SALA {
+public class TB_SALA extends TB_EQUI_SALA{
 	
-	private int CD_ID;
-    private String DT_DATA;
-    private String DT_HORA;
-	private int TB_TIPOAGENDAMENTO_CD_ID;
-	private int TB_FUNCIONARIO_CD_ID;
-	private int TB_EQUI_SALA_CD_ID;
-	private int TB_PACIENTE_CD_ID;
-	private int TB_ENTRADASAIDA_CD_ID;
+	private int CD_ID;	
+	private String NM_NUMERO;
+	private char Bl_OCUPADA;
+	private int TB_TIPOSALA_CD_ID;
+	
+	public TB_SALA(int CD_ID) {		
+		super(CD_ID);
+		this.NM_NUMERO = NM_NUMERO;
+		this.Bl_OCUPADA = Bl_OCUPADA;
+		this.TB_TIPOSALA_CD_ID = TB_TIPOSALA_CD_ID;
+	}
+	
+	public String getNM_NUMERO() {
+		return this.NM_NUMERO;
+	}
+	
+	public void setNM_NUMERO(String NM_NUMERO) {
+		this.NM_NUMERO=NM_NUMERO;
+	}
+	
+	public char getBl_OCUPADA() {
+		return this.Bl_OCUPADA;
+	}
+	
+	public void setBl_OCUPADA(char Bl_OCUPADA) {
+		this.Bl_OCUPADA=Bl_OCUPADA;
+	}
+	
+	public int getTB_TIPOSALA_CD_ID() {
+		return this.TB_TIPOSALA_CD_ID;
+	}
+	
+	public void setTB_TIPOSALA_CD_ID(int TB_TIPOSALA_CD_ID) {
+		this.TB_TIPOSALA_CD_ID=TB_TIPOSALA_CD_ID;
+	}
 
-    public void TB_AGENDAMENTO_PK(int CD_ID) {
+    public void TB_SALA_PK(int CD_ID) {
     	
     }
-	
-	public int getCD_ID() {
-		return this.CD_ID;
-	}
-	
-	public void setCD_ID(int CD_ID) {
-		this.CD_ID=CD_ID;
-	}
-	
-	public String getDT_DATA() {
-		return this.DT_DATA;
-	}
-	
-	public void setDT_DATA(String DT_DATA) {
-		this.DT_DATA=DT_DATA;
-	}
-	
-	public String getDT_HORA() {
-		return this.DT_HORA;
-	}
-	
-	public void setDT_HORA(String DT_HORA) {
-		this.DT_HORA=DT_HORA;
-	}
-	
-	public int getTB_TIPOAGENDAMENTO_CD_ID() {
-		return this.TB_TIPOAGENDAMENTO_CD_ID;
-	}
-	
-	public void setTB_TIPOAGENDAMENTO_CD_ID(int TB_TIPOAGENDAMENTO_CD_ID) {
-		this.TB_TIPOAGENDAMENTO_CD_ID=TB_TIPOAGENDAMENTO_CD_ID;
-	}
-	
-	public int getTB_FUNCIONARIO_CD_ID() {
-		return this.TB_FUNCIONARIO_CD_ID;
-	}
-	
-	public void setTB_FUNCIONARIO_CD_ID(int TB_FUNCIONARIO_CD_ID) {
-		this.TB_FUNCIONARIO_CD_ID=TB_FUNCIONARIO_CD_ID;
-	}
-	
-	public int getTB_EQUI_SALA_CD_ID() {
-		return this.TB_EQUI_SALA_CD_ID;
-	}
-	
-	public void setTB_EQUI_SALA_CD_ID(int TB_EQUI_SALA_CD_ID) {
-		this.TB_EQUI_SALA_CD_ID=TB_EQUI_SALA_CD_ID;
-	}
-	
-	public int getTB_PACIENTE_CD_ID() {
-		return this.TB_PACIENTE_CD_ID;
-	}
-	
-	public void setTB_PACIENTE_CD_ID(int TB_PACIENTE_CD_ID) {
-		this.TB_PACIENTE_CD_ID=TB_PACIENTE_CD_ID;
-	}
-	
-	public int getTB_ENTRADASAIDA_CD_ID() {
-		return this.TB_ENTRADASAIDA_CD_ID;
-	}
-	
-	public void setTB_ENTRADASAIDA_CD_ID(int TB_ENTRADASAIDA_CD_ID) {
-		this.TB_ENTRADASAIDA_CD_ID=TB_ENTRADASAIDA_CD_ID;
-	}
-	public TB_TIPOAGENDAMENTO (int CD_ID, date DT_DATA, char DT_HORA, int TB_TIPOAGENDAMENTO_CD_ID, int TB_FUNCIONARIO_CD_ID, int TB_EQUI_SALA_CD_ID, int TB_PACIENTE_CD_ID, int TB_ENTRADASAIDA_CD_ID) {
-		this.CD_ID = CD_ID;
-		this.DT_DATA = DT_DATA;
-		this.DT_HORA = DT_HORA;
-		this.TB_TIPOAGENDAMENTO_CD_ID = TB_TIPOAGENDAMENTO_CD_ID;
-		this.TB_FUNCIONARIO_CD_ID = TB_FUNCIONARIO_CD_ID;
-		this.TB_EQUI_SALA_CD_ID = TB_EQUI_SALA_CD_ID;
-		this.TB_PACIENTE_CD_ID = TB_PACIENTE_CD_ID;
-		this.TB_ENTRADASAIDA_CD_ID = TB_ENTRADASAIDA_CD_ID;		
-	}
+
 
 }
